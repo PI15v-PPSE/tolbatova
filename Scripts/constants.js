e17e43ec14b47d48be199ed04ba32b18e1941b97
Anna Tolbatova <36916390+tolbatova@users.noreply.github.com>
2018-02-28 14:34:30 +0300

    Constants v0.1.1

--- Scripts/constants.js
+++ Scripts/constants.js
@@ -4,3 +4,23 @@
  * @class Constants
  * @type {string}
  **/
+/**
+ * Базовый путь
+ * @property BASEPATH
+ * @type {string}
+ **/
+var BASEPATH   = 'Content/';
+/**
+ * @property DIV
+ * @type {string}
+ **/
+var DIV        = '<div />';
+/**
+ * @property CLS_FIGURE
+ * @type {string}
+ **/
+var CLS_FIGURE = 'figure';
+/**
+ * @property CLS_MATTER
+ * @type {string}
+ */
