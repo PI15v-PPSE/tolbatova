447c91d809fcc622bea1f023bc7500e57fc421ee
Anna Tolbatova <36916390+tolbatova@users.noreply.github.com>
2018-02-28 14:35:02 +0300

    Constants v0.1.2

--- Scripts/constants.js
+++ Scripts/constants.js
@@ -24,3 +24,21 @@ var CLS_FIGURE = 'figure';
  * @property CLS_MATTER
  * @type {string}
  */
+var CLS_MATTER = 'matter';
+/**
+ * Направления марио
+ * @property directions
+ * @type {{none: number, left: number, up: number, right: number, down: number}}
+ * @param none {number} нет направления
+ * @param left {number} влево
+ * @param up {number} вверх
+ * @param right {number} вправо
+ * @param down {number} вниз
+ */
+var directions = {
+    none  : 0,
+    left  : 1,
+    up    : 2,
+    right : 3,
+    down  : 4
+};
