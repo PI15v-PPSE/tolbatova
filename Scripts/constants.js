526d03325b23a5aa4f1373cc413d9355125b9b64
Anna Tolbatova <36916390+tolbatova@users.noreply.github.com>
2018-02-28 14:35:43 +0300

    Constants v0.1.3

--- Scripts/constants.js
+++ Scripts/constants.js
@@ -42,3 +42,70 @@ var directions = {
     right : 3,
     down  : 4
 };
+/**
+ * Состояния марио обычное/гриб или после цветка
+ * @property mario_states
+ * @type {{normal: number, fire: number}}
+ * @param normal {number} нормальное состояние марио
+ * @param fire {number} состояние стреляющего марио
+ */
+var mario_states = {
+    normal : 0,
+    fire  : 1
+};
+/**
+ * Размер марио под грибом и без него
+ * @property size_states
+ * @type {{small: number, big: number}}
+ * @param small {number} маленький марио
+ * @param big {number} большой марио
+ */
+var size_states = {
+    small : 1,
+    big   : 2
+};
+/**
+ * Блоки
+ * @property ground_blocking
+ * @type {{none: number, left: number, top: number, right: number, bottom: number, all: number}}
+ * @param none {number} нет направления
+ * @param left {number} влево блок
+ * @param top {number} вверх блок
+ * @param right {number} вправо блок
+ * @param bottom {number} вниз блок
+ * @param all {number} блок везде
+ */
+var ground_blocking = {
+    none   : 0,
+    left   : 1,
+    top    : 2,
+    right  : 4,
+    bottom : 8,
+    all    : 15
+};
+/**
+ * Виды смертей (100% смерть и просто понижение лвла)
+ * @property death_modes
+ * @type {{normal: number, shell: number}}
+ * @param normal {number} обычная смерть
+ * @param shell {number} сброс мощности марио
+ */
+var death_modes = {
+    normal : 0,
+    shell  : 1
+};
+/**
+ * Пути к файлам со спрайтами
+ * @property images
+ * @type {{enemies: string, sprites: string, objects: string, peach: string}}
+ * @param enemies {string} Путь к изображениям с врагами
+ * @param sprites {string} Путь к изображениям с марио
+ * @param objects {string} Путь к изображениям с объектами
+ * @param peach {string} Путь к изображениям с принцессой
+ */
+var images = {
+    enemies : BASEPATH + 'mario-enemies.png',
+    sprites : BASEPATH + 'mario-sprites.png',
+    objects : BASEPATH + 'mario-objects.png',
+    peach   : BASEPATH + 'mario-peach.png'
+};
