8dd1ae8e37c1e73027b8a2bd02f842af5be192b2
Anna Tolbatova <36916390+tolbatova@users.noreply.github.com>
2018-02-28 14:43:59 +0300

    keys v0.3

--- Scripts/keys.js
+++ Scripts/keys.js
@@ -17,6 +17,37 @@
             return keys.handler(event, false);
         });
     },
+	/**
+     * Обработчик для клавиатуры, обработка кнопок
+     * @method handler
+     * @param event событие
+     * @param status статус события
+     * @returns {boolean}
+     */
+    handler : function(event, status) {
+        switch(event.keyCode) {
+            case 17://CTRL
+                keys.accelerate = status;
+                break;
+            case 40://DOWN ARROW
+                keys.down = status;
+                break;
+            case 39://RIGHT ARROW
+                keys.right = status;
+                break;
+            case 37://LEFT ARROW
+                keys.left = status;
+                break;
+            case 38://UP ARROW
+                keys.up = status;
+                break;
+            default:
+                return true;
+        }
+            
+        event.preventDefault();
+        return false;
+    },
     accelerate : false,
     left : false,
     up : false,
