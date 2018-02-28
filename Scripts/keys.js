a1afc7db817ac1a9188ef8a3579e855dda12af8f
Anna Tolbatova <36916390+tolbatova@users.noreply.github.com>
2018-02-28 14:31:41 +0300

    keys v0.1.1

--- Scripts/keys.js
+++ Scripts/keys.js
@@ -1,4 +1,18 @@
 /**
  * Обработка клавиатуры
  * @module Клавиатура
+ * @class keys
  */
+ var keys = {
+    /**
+     * Связь с клавиатурой
+     * @method bind
+     * @return {handler} обработчик для клавиатуры
+     */
+    
+    accelerate : false,
+    left : false,
+    up : false,
+    right : false,
+    down : false
+};
